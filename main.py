6e2e9ef815171be21c790936fcf5a36ef6eb57ae
marlon fischer <marlon.fischer@gmail.com>
2025-03-23 23:00:33 -0400

    Game loop 2

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    
+    clock = pygame.time.Clock()
+    dt = 0
+
     while(True):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -12,6 +14,8 @@ def main():
         
         pygame.Surface.fill(screen, (0, 0, 0))
         pygame.display.flip()
+        time_passed = clock.tick(60)
+        dt = time_passed / 1000
 
 if __name__ == "__main__":
     main()
